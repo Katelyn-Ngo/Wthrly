627940b09a86f6f01c4ac0d72bd1a5c6c9aafbf7
Katelyn Ngo <thienhuongngo13@gmail.com>
2024-06-20 00:12:44 +0000

    completed search bar and favorite icon.

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
+'use strict';
 const $weatherAppDiv = document.querySelector('[data-view="weather-app]');
 if (!$weatherAppDiv) throw new Error('weather app div query failed');
 const $form = $weatherAppDiv.querySelector('form');
-console.log($form);
